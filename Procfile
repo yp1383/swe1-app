a892ddccd653a03b1a70960e00cdc68120c46028
yp1383 <yp1383@nyu.edu>
2021-10-01 18:44:31 -0400

    asd

--- Procfile
+++ Procfile
@@ -1,2 +1,6 @@
 release: python3 manage.py migrate
+<<<<<<< Updated upstream
 web: gunicorn mysite.wsgi
+=======
+web: gunicorn mysite.wsgi
+>>>>>>> Stashed changes
--- mysite/settings.py
+++ mysite/settings.py
@@ -16,21 +16,19 @@ import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8m43734q6k)uann#_v=1(%0$d^0d!=_p(+g6nfpy88-r!n6hnv'
+SECRET_KEY = 'django-insecure-y2#z$et837l0(v@+o1p_@j2%15e$3xiwjaza*n8+4&y)2s61ol'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*'] #'scott-cannings-swe1-app.herokuapp.com'
 
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -120,7 +118,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# Extra places for collectstatic to find static files.
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
