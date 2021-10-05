6b644a9c9747401ab4a01b921cdcb84151caf053
yp1383 <70648748+yp1383@users.noreply.github.com>
2021-10-05 19:57:30 -0400

    Update settings.py

--- helloWorld/settings.py
+++ helloWorld/settings.py
@@ -118,9 +118,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+STATIC_ROOT = os.path.join(BASE_DIR,'static') 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR,'static') 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
