82e7c82b40559c8e5bb9d06c392798bc2a89382b
yp1383 <70648748+yp1383@users.noreply.github.com>
2021-10-21 13:08:28 -0400

    Update views.py

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponseRedirect
-from django.shortcuts import get_object_or_404, render
-from django.shortcuts import ender
+from django.shortcuts import get_object_or_404
+from django.shortcuts import render
 from django.urls import reverse
 from django.views import generic
 
